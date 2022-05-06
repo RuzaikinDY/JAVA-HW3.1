22e3c5dfc0cdcc3e3b3baffd771d41879f069605
RuzaikinDY <Devilwind@mail.ru>
2022-05-06 21:06:38 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    public int calculate(int price) {
+        int Bonusmiles = price / 20;
+
+        //return price / 20;
+        return Bonusmiles;
+    }
+
+
+}
